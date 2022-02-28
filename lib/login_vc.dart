ebd7acef197248f8bcfb7bd0846fd243abe65b0e
bhussainEvision <75012150+bhussainEvision@users.noreply.github.com>
2022-02-28 13:43:52 +0500

    SignIn Screen design done

--- lib/login_vc.dart
+++ lib/login_vc.dart
@@ -23,15 +23,129 @@ class _LoginVcState extends State<LoginVc> {
 
   void login(BuildContext context) {
     if (email.isEmpty) {
+      showAlert('Please enter your email', context);
       return;
     }
 
     if (pasword.isEmpty) {
+      showAlert('Please enter your password', context);
       return;
     }
+
+    
   }
 
-  void showAlertWithMessage(String msg) {}
+  void showAlert(String msg, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(''),
+        content: Text(msg),
+        actions: [
+          TextButton(
+              onPressed: (() => {Navigator.of(context).pop()}),
+              child: const Text('OK')),
+        ],
+      ),
+    );
+  }
+
+  Widget emailTextInputField() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      height: 50,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(width: 1, color: Colors.grey)),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: Image.asset('images/icon-email.png'),
+            width: 35,
+            height: 35,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 1,
+            child: Container(
+              width: 1,
+              height: 30,
+              color: Colors.grey,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Center(
+                child: TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter your email',
+                      contentPadding: EdgeInsets.only(bottom: 15.0)),
+                      keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget pwdTextInputField() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      height: 50,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(width: 1, color: Colors.grey)),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: Image.asset('images/icon-lock.png'),
+            width: 35,
+            height: 35,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 1,
+            child: Container(
+              width: 1,
+              height: 30,
+              color: Colors.grey,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Center(
+                child: TextField(
+                  onChanged: (value) {
+                    pasword = value;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter your password',
+                      contentPadding: EdgeInsets.only(bottom: 15.0)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,44 +166,26 @@ class _LoginVcState extends State<LoginVc> {
             const SizedBox(
               height: 200,
             ),
-            TextField(
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your email',
-                labelText: 'Username',
-              ),
-            ),
+            emailTextInputField(),
             const SizedBox(
-              height: 10,
+              height: 15,
             ),
-            TextField(
-              onChanged: (value) {
-                pasword = value;
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter your password',
-                labelText: 'Password',
-              ),
-            ),
+            pwdTextInputField(),
             const SizedBox(
-              height: 60,
+              height: 25,
             ),
-            Container(
-              decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(5))),
-              width: 280,
+            MaterialButton(
+              onPressed: () => login(context),
+              color: Colors.blue,
               height: 40,
-              child: TextButton(
-                onPressed: (() => login(context)),
-                child:
-                    Text('Sign in to my account', style: fontStyleSettings(16)),
+              child: const Text(
+                'Sign in to my account',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
               ),
-            ),
+            )
           ],
         ),
       ),
